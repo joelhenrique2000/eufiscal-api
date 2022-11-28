350ad269e2adfbf1dc20f6e98018d1cc3f0ec225
Thiago Hortencio Silva de Melo <tmelo@ionic.health>
2022-11-27 23:40:39 -0300

    Aumentar validade do token jwt - 1 hora

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { JwtModule } from '@nestjs/jwt';
      PassportModule,
      JwtModule.register({
         secret: process.env.JWT_SECRET,
-        signOptions: { expiresIn: '15min' },
+        signOptions: { expiresIn: '1h' },
       }),
     ],
   controllers: [AuthController],
